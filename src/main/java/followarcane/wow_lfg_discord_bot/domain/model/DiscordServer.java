13a76a6a05f28f41880104799f40f964ec09f454
followarcane <batuhanbiter@gmail.com>
2025-03-25 14:08:00 +0000

    Fix Column

--- src/main/java/followarcane/wow_lfg_discord_bot/domain/model/DiscordServer.java
+++ src/main/java/followarcane/wow_lfg_discord_bot/domain/model/DiscordServer.java
@@ -11,6 +11,7 @@ import java.util.Set;
 @Table(name = "discord_servers")
 public class DiscordServer {
     @Id
+    @Column(name = "server_id")
     private String serverId;
     private String serverName;
     private String ownerId;
